a5b34e9a4dd333fa52ad1d2d326644e416b2958b
elmkni <53652478+elmkni@users.noreply.github.com>
2023-07-08 14:33:43 +0200

    Update sheret.go
    bumped version from '1.2' to '1.22' to reflect the changes I made!

--- src/sheret.go
+++ src/sheret.go
@@ -22,7 +22,7 @@ import (
 
 const appname string ="Sheret"
 const appdesc string ="Static Web Server"
-const version string = "1.2"
+const version string = "1.22"
 
 func loggingHandler(h http.Handler, quiet bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
